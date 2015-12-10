69bbfd0cee2bd130924e3e2188b5d00c01fd47a0
Santiago Larrain <slarrain@uchicago.edu>
2015-12-09 20:47:01 -0600

    Need to add Create DB - DDL should be fine

---
+++ hwfiles/createDB1.sql
@@ -0,0 +1,54 @@
+CREATE TABLE client (
+    CLIENT_ID     INTEGER   PRIMARY KEY,
+    NAME          VARCHAR,
+    ADDRESS_1     VARCHAR,
+    ADDRESS_2     VARCHAR,
+    CITY          VARCHAR,
+    STATE         VARCHAR,
+    ZIP           INTEGER
+);
+
+CREATE TABLE compensation (
+  COMPENSATION_ID           INTEGER   PRIMARY KEY,
+  LOBBYIST_ID               INTEGER,
+  COMPENSATION_AMOUNT       VARCHAR,
+  CLIENT_ID                 INTEGER
+);
+
+CREATE TABLE employer (
+  EMPLOYER_ID   INTEGER   PRIMARY KEY,
+  NAME          VARCHAR,
+  ADDRESS_1     VARCHAR,
+  ADDRESS_2     VARCHAR,
+  CITY          VARCHAR,
+  STATE         VARCHAR,
+  ZIP           INTEGER
+);
+
+CREATE TABLE expenditures (
+  EXPENDITURE_ID            INTEGER   PRIMARY KEY,
+  LOBBYIST_ID               INTEGER,
+  ACTION                    VARCHAR,
+  AMOUNT                    VARCHAR,
+  EXPENDITURE_DATE          DATE,
+  PURPOSE                   VARCHAR,
+  RECIPIENT                 VARCHAR,
+  CLIENT_ID                 INTEGER
+);
+
+CREATE TABLE activity (
+  LOBBYING_ACTIVITY_ID      INTEGER   PRIMARY KEY,
+  ACTION_SOUGHT             VARCHAR,
+  DEPARTMENT                VARCHAR,
+  CLIENT_ID                 INTEGER,
+  LOBBYIST_ID               INTEGER
+);
+
+CREATE TABLE lobbyist (
+  LOBBYIST_ID               INTEGER   PRIMARY KEY,
+  EMPLOYER_ID               INTEGER,
+  CLIENT_ID                 INTEGER,
+  LOBBYIST_SALUTATION       VARCHAR,
+  LOBBYIST_FIRST_NAME       VARCHAR,
+  LOBBYIST_LAST_NAME        VARCHAR
+);
